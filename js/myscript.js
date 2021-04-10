a85dd25b2dad3236fb14ef05ea0b86656dfbc92d
Vitor Garcia <vitor.stabile@hotmail.com>
2021-04-09 22:43:50 -0300

    Starting a todo list with JS

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,10 @@
-alert("Hello World!");
+// to make a search in the div of a element you wnat to search
+// use document.queruSelector("id of the main element", "The element you want")
+
+var listElement = document.querySelector('#app ul');
+
+var inputElement = document.querySelector('#app input');
+
+var buttonElement = document.querySelector('#app button');
+
+var todos = ['Go to Market','Learn JS', 'Pay the Bills'];
